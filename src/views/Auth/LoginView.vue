d249146fa827f6fa2bfce7930f7cf67fb612f123
Emir Dzhalalov <dzalalovemran@gmail.com>
2026-04-09 01:19:25 +0300

    Верстка страниц логина, регистрации и главной страницы

--- src/views/Auth/LoginView.vue
+++ src/views/Auth/LoginView.vue
@@ -1,7 +1,43 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <h1>Login Page</h1>
+      <v-col cols="12" sm="8" lg="6">
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Login</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form v-model="valid" ref="form" lazy-validation>
+              <v-text-field
+                prepend-icon="mdi-account"
+                name="email"
+                label="Email"
+                type="email"
+                v-model="email"
+                :rules="emailRules"
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="mdi-lock"
+                name="password"
+                label="Password"
+                type="password"
+                v-model="password"
+                :rules="passwordRules"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              @click="onSubmit"
+              :disabled="!valid"
+            >
+              Login
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -10,6 +46,28 @@
 export default {
   data () {
     return {
+      email: "",
+      password: "",
+      valid: false,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 6) || 'Password must be more or equal than 6 characters'
+      ]
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.$refs.form.validate()) {
+        const user = {
+          email: this.email,
+          password: this.password
+        }
+        console.log(user)
+      }
     }
   }
 }
