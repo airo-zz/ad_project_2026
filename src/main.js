a67f1bace5408331fe520cf2d9e057bbf05cb007
Emir Dzhalalov <dzalalovemran@gmail.com>
2026-04-09 00:50:26 +0300

    Установка пакетов vuetify и router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(router).use(vuetify).mount('#app')
